04ddcbdf50d61c944e1f8eacceeb25db7047fa0b
lani <laniiiiworld@gmail.com>
2022-09-21 09:46:17 +0900

    PostCSS

---
+++ postCSS/src/components/app.jsx
@@ -0,0 +1,14 @@
+import '../app.css';
+import Button1 from './button1';
+import Button2 from './button2';
+
+function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+    </>
+  );
+}
+
+export default App;
