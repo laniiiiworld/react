7ad074ba7a531c299a472065df5c6a8886757503
lani <laniiiiworld@gmail.com>
2022-11-17 11:01:54 +0900

    기본 내용 정리(Products - useEffect)

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './AppCounter';
+import App from './AppProducts';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
