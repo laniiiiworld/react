5ec1f660015481af5ff071f9680a502d0da42dc8
lani <laniiiiworld@gmail.com>
2022-12-03 11:04:18 +0900

    CSS라이브러리(PostCSS, Styled Components, Tailwind)

---
+++ CSS/src/TailwindComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TailwindComponent() {
+  return (
+    <div>
+      <h1 className='text-4xl'>Hello!</h1>
+      <button className='bg-blue-300 rounded-xl px-2'>Button</button>
+    </div>
+  );
+}
---
+++ CSS/src/app.jsx
@@ -0,0 +1,18 @@
+import './app.css';
+import Button1 from './components/button1';
+import Button2 from './components/button2';
+import StyledComponent from './StyledComponent';
+import TailwindComponent from './TailwindComponent';
+
+function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+      <StyledComponent />
+      <TailwindComponent />
+    </>
+  );
+}
+
+export default App;
